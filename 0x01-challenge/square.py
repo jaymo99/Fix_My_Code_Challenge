9850493adcf67decb458c79f33e82a4851bc33fe
James Kariuki <karisjaymo99@gmail.com>
2023-07-17 22:13:31 +0300

    fix typos

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -16,7 +16,7 @@ class Square():
         """ Area of the square """
         return self.width * self.height
 
-    def perimiter_of_my_square(self):
+    def permiter_of_my_square(self):
         """ Perimeter of the square """
         return (self.width * 2) + (self.height * 2)
 
